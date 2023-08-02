e3ea2215ecae3103397f21272609dac6a8eee9b4
h4ckua11 <h4ckua11@gmx.net>
2023-08-02 17:08:19 +0200

    Added check if local var exists

--- moveWar.sh
+++ moveWar.sh
@@ -1,1 +1,1 @@
-sudo cp guacamole/target/guacamole-1.5.0.war /var/lib/tomcat9/webapps/guacamole.war && sudo service tomcat9 restart
+sudo cp guacamole/target/guacamole-1.5.3.war /var/lib/tomcat9/webapps/guacamole.war && sudo service tomcat9 restart
